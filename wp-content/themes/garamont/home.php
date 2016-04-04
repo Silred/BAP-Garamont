bdc5226a57296a12b4715ffbf4becdd6ec23bd63
Delahais <mdelahais1@gmail.com>
2016-04-04 22:41:24 +0200

    Home modificated

--- wp-content/themes/garamont/home.php
+++ wp-content/themes/garamont/home.php
@@ -11,7 +11,40 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
 
-/** Shortcode pour un slider ou une galerie */
+<!-- Mettre une sidebar boostrap ou pas ->
+
+<!--    <nav class="navbar navbar-default" role="navigation">
+
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
+        </div>
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>-->
+
+<h1>Lycée Garamont</h1>
+<h2>Description du Lycée</h2>
+
+    /** Shortcode pour un slider ou une galerie */
 
 
 <?php get_footer(); ?>
