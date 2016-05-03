cbbc2a7767454940e40b9ec4a47bd7f4e1dac1f9
Silred <debray.c.simon@gmail.com>
2016-05-03 14:33:27 +0200

    Home debug création footer

--- wp-content/themes/garamont/footer.php
+++ wp-content/themes/garamont/footer.php
@@ -8,7 +8,29 @@
 ?>
 
 <div class="row  footer">
-    Copyright Lycée Claude Garamont @ 2016 All rights reserved
+
+    <div class="col-md-1"></div>
+    <div class="col-md-3">
+
+        <h3>Liens utiles</h3>
+
+        <a href="http://www.ac-versailles.fr/public/jcms/c_5012/accueil">Académie de Versailles</a>
+        <a href="http://www.admission-postbac.fr/">APB (Admission Post Bac)</a>
+        <a href="https://0922427n.index-education.net/pronote/">Pronote</a>
+        <a href="http://www.7etapespourtrouverunstage.com/entrainement.html">Trouver un stage</a>
+
+    </div>
+
+
+    <div class="col-md-8"></div>
+
+    <div class="col-md-12">
+        <p>Copyright Lycée Claude Garamont @ 2016 All rights reserved</p>
+    </div>
+
+
+
+
 </div>
 
 <script>
