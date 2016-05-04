a1b87a7aac0ff36a0222906daae8261a8d4b8762
Silred <debray.c.simon@gmail.com>
2016-05-04 14:25:51 +0200

    Mise a jour footer

--- wp-content/themes/garamont/footer.php
+++ wp-content/themes/garamont/footer.php
@@ -10,7 +10,19 @@
 <div class="row  footer">
 
     <div class="col-md-1"></div>
+
     <div class="col-md-3">
+
+        <img src="http://simondebray.fr/garamont/wp-content/uploads/2016/04/Sans-titre-1.png" alt=""/>
+
+        <p>Téléphone : 01.41.57.32.20</p>
+
+        <p>Mail :  contact@garamont.fr</p>
+
+
+    </div>
+
+    <div class="col-md-2 footer-lien-utile">
 
         <h3>Liens utiles</h3>
 
@@ -21,14 +33,40 @@
 
     </div>
 
+    <div class="col-md-3">
 
-    <div class="col-md-8"></div>
+        <h3>LYCÉE CLAUDE GARAMONT</h3>
+
+        <p>69 rue de l'industrie 92707 Colombes</p>
+
+        <p>Bus : Ligne 176</p>
+
+        <p>Arrêt : Rue de l'industrie</p>
+
+        <p>Direction : Petit Gennevilliers</p>
+
+        <p>Train : Gare de la Garenne Colombes et/ou Gare des Vallées</p>
+
+    </div>
+
+    <div class="col-md-2">
+
+        <h3>HORRAIRES DU LYCÉE</h3>
+
+        <p>Lundi - Vendredi : 8h - 19h</p>
+
+        <p>Samedi : 9h - 13h</p>
+
+    </div>
+
+    <div class="col-md-1"></div>
 
     <div class="col-md-12">
-        <p>Copyright Lycée Claude Garamont @ 2016 All rights reserved</p>
+        <p class="footer-legal">Copyright Lycée Claude Garamont @ 2016 All rights reserved</p>
     </div>
 
 </div>
+
 
 <script>
     jQuery(function($) {
